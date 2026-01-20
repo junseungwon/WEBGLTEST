27b6d21f056bf0d1a700dcf77077bcf25ffa0ffa
Junseungwon <zkdlman1@gmail.com>
2026-01-21 02:01:18 +0900

    ds

--- Build/01.KafkaFantasy.framework.js
+++ Build/01.KafkaFantasy.framework.js
@@ -1246,29 +1246,29 @@ var tempDouble;
 var tempI64;
 
 var ASM_CONSTS = {
- 2789912: function() {
+ 2789880: function() {
   return Module.webglContextAttributes.premultipliedAlpha;
  },
- 2789973: function() {
+ 2789941: function() {
   return Module.webglContextAttributes.preserveDrawingBuffer;
  },
- 2790037: function() {
+ 2790005: function() {
   return Module.webglContextAttributes.powerPreference;
  },
- 2790095: function() {
+ 2790063: function() {
   Module["emscripten_get_now_backup"] = performance.now;
  },
- 2790150: function($0) {
+ 2790118: function($0) {
   performance.now = function() {
    return $0;
   };
  },
- 2790198: function($0) {
+ 2790166: function($0) {
   performance.now = function() {
    return $0;
   };
  },
- 2790246: function() {
+ 2790214: function() {
   performance.now = Module["emscripten_get_now_backup"];
  }
 };
